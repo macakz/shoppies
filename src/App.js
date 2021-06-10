d225d186cbee017585d6e4fa7ab0bb21956b8e37
Alexander McKerrow <mckerrow4@gmail.com>
2021-06-10 16:29:54 +1200

    can hardcode searches to log

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import axios from 'axios'
 
-function App() {
+function App () {
+
+  const getMovie = async () => {
+
+    let movieData = await axios.get("http://www.omdbapi.com/?apikey=9ded3126&s=band of brothers")
+    console.log(movieData.data.Search)
+  }
+
+  getMovie()
   return (
     <div className="App">
-      <div>testing</div>
+      <h1>The Shoppies</h1>
     </div>
   );
 }
