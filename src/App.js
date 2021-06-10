97335909459f8673c93ed146e0db69e7db103b67
Alexander McKerrow <mckerrow4@gmail.com>
2021-06-10 19:06:30 +1200

    adding some styling

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App () {
             :
             searchResultData.map((item, index) => {
               return (
-                <div>
+                <div class="container">
                   <a href={`https://www.imdb.com/title/${item.imdbID}`}>
                     <h1>Title: {item.Title}</h1>
                     <h2> Released: {item.Year}</h2>
